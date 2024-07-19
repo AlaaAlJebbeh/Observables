3c7e106af96174654af4b8a8936b5bdf3b485ee7
Alaa Al Jebbeh <alaa.jeebeh@gmail.com>
2024-07-19 13:25:54 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { interval, map } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit{
+  clickCount = signal(0);
+  private destroyRef = inject(DestroyRef);
+
+  constructor(){
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`)
+    });
+  }
+
+  ngOnInit(){
+/*     const subscribtion = interval(1000).pipe(
+      map((val)=> val * 2)
+    ).subscribe({
+      next: (val) => console.log(val),
+      error: (val) => console.log(val)
+    });
+
+    this.destroyRef.onDestroy(()=>{
+      subscribtion.unsubscribe();
+    }); */
+  }
+
+  onClick(){
+    this.clickCount.update(prevCount => prevCount + 1);
+
+  }
+
+}
